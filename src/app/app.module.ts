6b01707283f0703288240aed0e9633c1fd363dae
Karty <karthick.hock@gmail.com>
2018-01-12 20:07:22 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from "@angular/router";
 import { ProfileService } from "./profile/profile.service";
+import {ChipsModule} from 'primeng/primeng';
+
+
+
 
 //import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 //import { InMemoryDataService } from './in-memory-data.service';
@@ -16,13 +21,15 @@ import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
+import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
 import { DateConverterPipe } from './date-converter.pipe';
 
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "dashboard", pathMatch: "full" },
   { path: "dashboard", component: DashboardComponent },
-  { path: "profile-detail/:id", component: ProfileDetailComponent }
+  { path: "profile-detail/:id", component: ProfileDetailComponent },
+  { path: "profile-edit", component: ProfileEditComponent }
 
 ];
 
@@ -36,12 +43,16 @@ const appRoutes: Routes = [
     DashboardComponent,
     ProfileComponent,
     ProfileDetailComponent,
-    DateConverterPipe
+    ProfileEditComponent,
+    DateConverterPipe,
+
   ],
  imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    ChipsModule,
     
     RouterModule.forRoot(
       appRoutes
